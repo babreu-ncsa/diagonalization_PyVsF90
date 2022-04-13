0833852b2e2774dfb03a3540db9d0262a2783189
Bruno Ricardi de Abreu <babreu@dt-login01.delta.internal.ncsa.edu>
2022-04-13 11:01:14 -0500

    collecting all data

--- timing_diag.py
+++ timing_diag.py
@@ -1,19 +1,23 @@
 import numpy as np
+import pandas as pd
 from scipy.linalg import eigh
 import time
 
 its = 5
 N = 200
 samples = 100
-t = its*[0]
+t = np.zeros(samples)
+df = pd.DataFrame()
 
 for i in range(its):
     for j in range(samples):
         x = np.random.rand(N,N)
-        s = time.time()
+        s = time.perf_counter()
         w, v = eigh(x, lower=False)
-        e = time.time()
-        t[i] = t[i] + (e - s)
+        e = time.perf_counter()
+        t[j] = (e - s)
+    
+    df[str(N)] = t
     N = N*2
 
-print(t, sum(t))
+df.to_csv("./py-diag-times.csv")
